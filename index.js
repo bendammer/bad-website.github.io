c637f937eadb49c0472595997748289063c03ce9
ben <b.a.dammer@googlemail.com>
2020-03-17 17:02:07 +0100

    deleted console logs

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const hasDropdown = document.querySelectorAll('.has-dropdown');
-console.log(hasDropdown);
 
 hasDropdown.forEach((element) => {
   element.addEventListener('click', (event) => {
     event.preventDefault();
     const dropdown = element.querySelector('.dropdown-menu');
-    console.log(dropdown);
     dropdown.classList.toggle('active');
   })
 });
