8fb9054ae452e1ceb10d28247a10450acf4df23e
ben <b.a.dammer@googlemail.com>
2020-03-20 13:17:06 +0100

    add images / changed font / minor changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const hasDropdown = document.querySelectorAll('.has-dropdown');
 hasDropdown.forEach((element) => {
   element.addEventListener('click', (event) => {
     event.preventDefault();
-    const dropdown = element.querySelector('.dropdown-menu');
+    const dropdown = element.parentElement.querySelector('.dropdown-menu');
     dropdown.classList.toggle('active');
   })
 });
