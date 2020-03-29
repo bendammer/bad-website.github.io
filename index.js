729a315481bd776a7767169ebce56870ed656966
ben <b.a.dammer@googlemail.com>
2020-03-29 14:53:09 +0200

    mobile-menu

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const hasDropdown = document.querySelectorAll('.has-dropdown');
+const hamburger = document.querySelector('.hamburger');
+const mobileMenu = document.querySelector('.mobile-menu');
+const exitButton = document.querySelector('.exit-button');
 
 hasDropdown.forEach((element) => {
   element.addEventListener('click', (event) => {
@@ -7,3 +10,13 @@ hasDropdown.forEach((element) => {
     dropdown.classList.toggle('active');
   })
 });
+
+hamburger.addEventListener('click', () => {
+  mobileMenu.style.display = 'flex';
+  hamburger.style.display = 'none';
+});
+
+exitButton.addEventListener('click', () => {
+  mobileMenu.style.display = 'none';
+  hamburger.style.display = 'block';
+})
