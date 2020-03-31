27ddf717fc013d8d0f1c25d2e1f70c53b9e8e9ae
ben <b.a.dammer@googlemail.com>
2020-03-31 17:23:16 +0200

    remove active class when exit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const hamburger = document.querySelector('.hamburger');
 const mobileMenu = document.querySelector('.mobile-menu');
 const exitButton = document.querySelector('.exit-button');
 const body = document.querySelector('body');
+const html = document.querySelector('html');
 
 hasDropdown.forEach((element) => {
   element.addEventListener('click', (event) => {
@@ -16,10 +17,16 @@ hamburger.addEventListener('click', () => {
   mobileMenu.style.display = 'flex';
   hamburger.style.display = 'none';
   body.style.overflowY = 'hidden';
+  /* body.style.position = 'relative'; */
+  html.style.overflowY = 'hidden';
+  /* html.style.position = 'relative'; */
 });
 
 exitButton.addEventListener('click', () => {
   mobileMenu.style.display = 'none';
   hamburger.style.display = 'block';
   body.style.overflowY = 'visible';
+  const dropdown = document.querySelector('.dropdown-menu');
+  dropdown.classList.remove('active');
+  /* html.style.overflowY = 'visible'; */
 })
