fedbdffebe7de1154a0ddf62e3a507825403a357
ben <b.a.dammer@googlemail.com>
2020-05-31 13:39:59 +0200

    added styling to theme buttons

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const hamburger = document.querySelector('.hamburger');
 const mobileMenu = document.querySelector('.mobile-menu');
 const exitButton = document.querySelector('.exit-button');
 const body = document.querySelector('body');
-const themeButton = document.querySelector('#theme-button');
+const darkButton = document.querySelector('#theme-button-dark');
+const lightButton = document.querySelector('#theme-button-light');
 const darkTheme = document.querySelector('.dark');
 
 hasDropdown.forEach((element) => {
@@ -28,8 +29,16 @@ exitButton.addEventListener('click', () => {
   dropdown.classList.remove('active');
 });
 
-themeButton.addEventListener('click', () => {
-  body.classList.toggle('dark');
+darkButton.addEventListener('click', () => {
+  body.classList.add('dark');
+  darkButton.style.display = 'none';
+  lightButton.style.display = 'block';
+});
+
+lightButton.addEventListener('click', () => {
+  body.classList.remove('dark');
+  darkButton.style.display = 'block';
+  lightButton.style.display = 'none';
 });
 
 
